815baa28065a4444ab4a431aa097b7529c6046dd
ella-chetvertak <olegcetvertak037@gmail.com>
2022-06-05 11:42:01 +0300

    strategy, team, review article, slider, adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,23 @@ let header = document.querySelector("#header");
 burger.onclick = function() {
     header.classList.toggle("menu-open");
 }
+
+// Carousel
+
+$(document).ready(function() {
+    $('.carousel').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 376,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });     
+});
