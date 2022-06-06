a577bcc62693d5d0a55fcd94646915e6925f35b6
ella-chetvertak <olegcetvertak037@gmail.com>
2022-06-06 15:42:39 +0300

    footer and full project

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,21 @@ $(document).ready(function() {
         ]
     });     
 });
+
+// Arrow top
+
+var smoothJumpUp = function() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      window.scrollBy(0,-50);
+      setTimeout(smoothJumpUp, 0.1);
+  }
+}
+
+window.onscroll = function() {
+var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+if (scrolled > 100) {
+  document.getElementById('upbutton').style.opacity = '1';
+} else {
+  document.getElementById('upbutton').style.opacity = '0';
+}
+}
